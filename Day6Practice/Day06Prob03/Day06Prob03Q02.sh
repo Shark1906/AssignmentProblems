875dc9c0a9da81ee6decff39825cadc8aa68497d
Shark1906 <amanraj1906@gmail.com>
2021-10-05 00:50:06 +0530

    Changes in Day06Prob03Q02 and added Day06Prob03Q03

--- Day6Practice/Day06Prob03/Day06Prob03Q02.sh
+++ Day6Practice/Day06Prob03/Day06Prob03Q02.sh
@@ -2,10 +2,11 @@ echo "Enter 1st Number";
 read num1;
 echo "Enter 2nd Number";
 read num2;
-sum=0;
+
 
 function palindrome(){
-
+sum=0;
+temp=$num;
     while [ $num -gt 0 ]
     do
         rem=$(($num%10));
@@ -26,13 +27,11 @@ fi
 if [ $num1 -gt -1 ]
 then
     num=$num1
-    temp=$num;
     palindrome;
 fi
 
 if [ $num2 -gt -1 ]
 then
     num=$num2;
-    temp=$num;
     palindrome;
 fi
