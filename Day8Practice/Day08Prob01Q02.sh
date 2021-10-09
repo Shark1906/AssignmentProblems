c29773b398cc8b48885bc97bcab7879ace0250e5
Shark1906 <amanraj1906@gmail.com>
2021-10-09 10:43:00 +0530

    Modifying in day8 practice

--- Day8Practice/Day08Prob01Q02.sh
+++ Day8Practice/Day08Prob01Q02.sh
@@ -25,84 +25,72 @@ while [[ $total -lt 50 ]]
         then
         ((jan++));
         birthmonth[jan]=$jan;
-        #echo "" ${dice[one]};
         fi
 
         if [ $newnum -eq 2 ]
         then
         ((feb++));
         birthmonth[feb]=$feb;
-        #echo "two" ${dice[two]};
         fi
 
         if [ $newnum -eq 3 ]
         then
         ((mar++));
         birthmonth[mar]=$mar;
-        #echo "three" ${dice[three]};
         fi
 
         if [ $newnum -eq 4 ]
         then
         ((apr++));
         birthmonth[apr]=$apr;
-        #echo "four" ${dice[four]};
         fi
 
         if [ $newnum -eq 5 ]
         then
         ((may++));
         birthmonth[may]=$may;
-        #echo "five" ${dice[five]};
         fi
 
         if [ $newnum -eq 6 ]
         then
         ((june++));
         birthmonth[june]=$june;
-        #echo "six" ${dice[six]};
         fi
 
         if [ $newnum -eq 7 ]
         then
         ((july++));
         birthmonth[july]=$july;
-        #echo "one" ${dice[one]};
         fi
 
         if [ $newnum -eq 8 ]
         then
         ((aug++));
         birthmonth[aug]=$aug;
-        #echo "two" ${dice[two]};
         fi
 
         if [ $newnum -eq 9 ]
         then
         ((sep++));
         birthmonth[sep]=$sep;
-        #echo "three" ${dice[three]};
         fi
 
         if [ $newnum -eq 10 ]
         then
         ((oct++));
         birthmonth[oct]=$oct;
-        #echo "four" ${dice[four]};
         fi
 
         if [ $newnum -eq 11 ]
         then
         ((nov++));
         birthmonth[nov]=$nov;
-        #echo "five" ${dice[five]};
         fi
 
         if [ $newnum -eq 12 ]
         then
         ((dec++));
         birthmonth[dec]=$dec;
-        #echo "six" ${dice[six]};
         fi
     
         fi
